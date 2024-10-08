f8a050a21ac23a88cb0bf0b731dd2799122f12a4
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-08 17:13:55 +0300

    class PartTimeEmployee

---
+++ task 3/PartTimeEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class PartTimeEmployee
+    {
+        public string Name { get; }
+        public int WeeklyHours { get; } = 20;
+
+        public PartTimeEmployee(string name)
+        {
+            Name = name;
+        }
+    }
+}
